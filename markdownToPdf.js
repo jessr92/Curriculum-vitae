46ca27c49cc81f07a75e8638faa0276b117072ae
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-03-20 22:39:01 +0000

    Ran IDE formatter over code

--- markdownToPdf.js
+++ markdownToPdf.js
@@ -1,17 +1,24 @@
-var markdownpdf = require('markdown-pdf')
-var path = require('path')
+/*jshint node: true*/
+"use strict";
+
+var markdownPDF = require("markdown-pdf");
+var path = require("path");
 
 var mdDocs = [
-  path.join(__dirname, 'gordon-reid-cv.md'),
-  path.join(__dirname, 'gordon-reid-transcript.md')
-]
+    path.join(__dirname, "gordon-reid-cv.md"),
+    path.join(__dirname, "gordon-reid-transcript.md")
+];
 
-var pdfDocs = mdDocs.map(function (d) { return d.replace('.md', '.pdf') })
+var pdfDocs = mdDocs.map(function (d) {
+    return d.replace(".md", ".pdf")
+});
 
 var options = {
-  cssPath: path.join(__dirname, 'custom.css')
-}
+    cssPath: path.join(__dirname, "custom.css")
+};
 
-markdownpdf(options).from(mdDocs).to(pdfDocs, function () {
-  pdfDocs.forEach(function (d) { console.log('Created', d) })
-})
+markdownPDF(options).from(mdDocs).to(pdfDocs, function () {
+    pdfDocs.forEach(function (d) {
+        console.log("Created", d)
+    })
+});
